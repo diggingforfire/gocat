7b1669d527e0efa5c24d110e3f6c97145cf5ff77
Robbie Luijben <robbie@luijben.tech>
2025-05-23 14:21:33 +0200

    align whitespace with go standards

--- gocat.go
+++ gocat.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 	"os"
 )
 
 func main() {
-    if len(os.Args) < 2 {
-        fmt.Println("Usage: gocat <filename>")
-        return
-    }
-    filename := os.Args[1]
-    data, err := os.ReadFile(filename)
-    if err != nil {
-        fmt.Println("Error reading file:", err)
-        return
-    }
-    fmt.Println(string(data))
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: gocat <filename>")
+		return
+	}
+	filename := os.Args[1]
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+	fmt.Println(string(data))
 }
