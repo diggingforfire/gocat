b8a81f7d6cc99829bed2d06e94a0bb482bccf142
Robbie Luijben <robbie@luijben.tech>
2025-05-23 14:29:05 +0200

    read from stdin if no file or single dash is passed

--- gocat.go
+++ gocat.go
@@ -6,8 +6,13 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: gocat <filename>")
+	if len(os.Args) < 2 || os.Args[1] == "-" {
+		input, err := os.ReadFile("/dev/stdin")
+		if err != nil {
+			fmt.Println("Error reading from stdin:", err)
+			return
+		}
+		fmt.Print(string(input))
 		return
 	}
 	filename := os.Args[1]
