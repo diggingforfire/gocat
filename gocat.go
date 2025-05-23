c125c388473e1c54a7caa79e9c62ce69dc8afe89
Robbie Luijben <robbie@luijben.tech>
2025-05-23 14:01:43 +0200

    Read filename from args (step 1 complete)

--- gocat.go
+++ gocat.go
@@ -6,10 +6,15 @@ import (
 )
 
 func main() {
- 	data, err := os.ReadFile("test.txt")
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
-	fmt.Println(string(data))
+    if len(os.Args) < 2 {
+        fmt.Println("Usage: gocat <filename>")
+        return
+    }
+    filename := os.Args[1]
+    data, err := os.ReadFile(filename)
+    if err != nil {
+        fmt.Println("Error reading file:", err)
+        return
+    }
+    fmt.Println(string(data))
 }
